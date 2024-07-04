c7b9f5742152f887afdea8c6e80707853565036e
Husan <shomurodovhusan130@gmail.com>
2024-07-05 00:59:40 +0500

    finish

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
+// const slideOne = document.getElementById("slide-1");
+
+// window.addEventListener("scroll", () => window.scrollY >= 30 ? slideOne.classList.add("active") : false)
+
 const slideOne = document.getElementById("slide-1");
-
-window.addEventListener("scroll", () => {
-    const triggerHeight = window.innerHeight / 1;
-    const slideTop = slideOne.getBoundingClientRect().top;
-
-    if (slideTop < triggerHeight) {
-        slideOne.classList.add("active");
-    } else {
-        slideOne.classList.remove("active");
-    }
+const runTitle = document.querySelector(".box-title")
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            setTimeout(() => {
+                slideOne.classList.add("active");
+                observer.unobserve(slideOne);
+            }, 1000)
+        }
+    });
 });
+
+observer.observe(slideOne);
+
+let fullText = runTitle.innerHTML
+
+runTitle.innerHTML = ''
+
+const runText = (x = 0) => {
+    runTitle.innerHTML = runTitle.innerHTML + fullText[x]
+    x++;
+    stop = setTimeout(() => runText(x), 200)
+    x == 26 ? clearTimeout(stop) : false
+}
+
+runText()
